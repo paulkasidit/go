29616861fa376599bf17ab841ca426cd1bd746bc
Paul Kasidit <paulkasidit@gmail.com>
2023-02-14 01:08:03 -0800

    changed file name

--- src/components/MainControl.js
+++ src/components/MainControl.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./App.css";
 import "./Main/Main.css"
-import AirBnbAPI from './Main/AirbnbAPI';
+import AirbnbAPI from './Main/AirbnbAPI';
 import GoogleMapsAPI from './Main/GoogleMapsAPI';
 
 class MainControl extends React.Component {
