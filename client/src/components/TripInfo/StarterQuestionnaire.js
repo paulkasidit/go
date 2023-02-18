f13f0839eb2dcaf06759c0f26c2fc94e80235a19
Paul Kasidit <paulkasidit@gmail.com>
2023-02-18 13:49:26 -0800

    finished starter questionnaire

--- client/src/components/TripInfo/StarterQuestionnaire.js
+++ client/src/components/TripInfo/StarterQuestionnaire.js
@@ -4,7 +4,93 @@ import "./TripInfo.css";
 function StarterQuestionnaire(){
   return(
     <React.Fragment>
-      <p>Hello</p>
+      <div class = "box">
+        <form>
+          <p>What are your interests?</p>
+          <div class = "control">
+              <label class = "radio">
+                <input 
+                  type = "radio"
+                  name = "tripInterests"
+                  value = "outdoors"
+                  id  = "outdoors"
+                />
+                Outdoors
+              </label>
+              <label class = "radio">
+                <input 
+                  type = "radio"
+                  name = "tripInterests"
+                  value = "nightlife"
+                  id  = "nightlife"
+                />
+                Nightlife
+              </label>
+              <label class = "radio">
+                <input 
+                  type = "radio"
+                  name = "tripInterests"
+                  value = "dining"
+                  id  = "dining"
+                />
+                Dining
+              </label>
+            </div>
+            <br/>
+            <p>What are your accomodation preferences?</p>
+            <div class = "control">
+              <label class = "radio">
+                <input 
+                  type = "radio"
+                  name = "accomodationPreference"
+                  value = "family"
+                  id  = "family"
+                />
+                Family
+              </label>
+              <label class = "radio">
+                <input 
+                  type = "radio"
+                  name = "accomodationPreference"
+                  value = "dining"
+                  id  = "dining"
+                />
+                Bachelor
+              </label>
+            </div>
+            <br/>
+            <p>Most importantly, what is your budget? </p>
+            <div class = "control">
+              <label class = "radio">
+                  <input 
+                    type = "radio"
+                    name = "accomodationBudget"
+                    value = "low"
+                    id  = "low"
+                  />
+                  $
+              </label>
+              <label class = "radio">
+                  <input 
+                    type = "radio"
+                    name = "accomodationBudget"
+                    value = "middle"
+                    id  = "middle"
+                  />
+                  $$
+              </label>
+              <label class = "radio">
+                  <input 
+                    type = "radio"
+                    name = "accomodationBudget"
+                    value = "high"
+                    id  = "high"
+                  />
+                  $$$
+              </label>
+            </div>
+        </form>
+      </div>
     </React.Fragment>
   )
 }
