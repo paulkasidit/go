586724ab59372decb62e418f5afc5ffe29e58041
Paul Kasidit <paulkasidit@gmail.com>
2023-02-18 01:36:36 -0800

    create Component starter Questionnaire

---
+++ client/src/components/Main/StarterQuestionnaire.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Main.css";  
+
+function StarterQuestionnaire(){
+  return(
+    <React.Fragment>
+      <p>Hello</p>
+    </React.Fragment>
+  )
+}
+
+export default StarterQuestionnaire
