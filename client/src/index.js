7c30463286ba27c1adfcbd332f764ef2911df7c2
Paul Kasidit <paulkasidit@gmail.com>
2023-02-19 14:10:22 -0800

    moved auth0 variables to env file

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
-    domain = "dev-vrnlarrsgx15gyss.us.auth0.com"
-    clientId = "I0TLQTHC4blE8JlSIXyp3BzTIpl1EgIk"
+    domain = {process.env.REACT_APP_AUTH0_DOMAIN }
+    clientId = {process.env.REACT_APP_AUTH0_CLIENT_ID}
     authorizationParams = {{
     redirect_uri: window.location.origin
   }}
